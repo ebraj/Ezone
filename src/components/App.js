b5f2dfe749c3e08e2012f1ed02ff2d594141bc93
Ebraj Gurung <gurungebraj22@gmail.com>
2021-10-12 21:29:15 +0545

    Navbar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+// Components
+import Navbar from "./Navbar";
+
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-900 py-5 px-6">
+      <nav className="lg:container mx-auto container text-gray-100 flex items-center justify-between">
+        <div>
+          <h1>EZONE</h1>
+        </div>
+        <ul className="flex items-center">
+          <li>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="#eeeeff"
+              >
+                <path d="M20 7h-4v-3c0-2.209-1.791-4-4-4s-4 1.791-4 4v3h-4l-2 17h20l-2-17zm-11-3c0-1.654 1.346-3 3-3s3 1.346 3 3v3h-6v-3zm-4.751 18l1.529-13h2.222v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h2.222l1.529 13h-15.502z" />
+              </svg>
+            </span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
